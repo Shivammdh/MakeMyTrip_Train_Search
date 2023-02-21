0c95c1cd1e361ecf3bc12240ca7639ef2076bf12
Shivammdh <67448585+Shivammdh@users.noreply.github.com>
2023-02-21 17:08:47 +0530

    Add files via upload

---
+++ Tests/Automaticaly_run_grid_using_Multithreading.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import time
+from threading import Thread
+
+class Hub(Thread):
+    def run(self) -> None:
+        os.chdir('D:\Selenium')
+        os.system('java -jar selenium-server-4.7.2.jar hub')
+
+
+class Node(Thread):
+    def run(self) -> None:
+        os.chdir('D:\Selenium')
+        os.system('java -jar selenium-server-4.7.2.jar node')
+
+class RunCode(Thread):
+    def run(self) -> None:
+        os.system(
+            'python D:\Train-Search-POM\Tests\RunTestInGrid.py')
+
+
+
+
+hubobj=Hub()
+nodeobj=Node()
+runcode=RunCode()
+hubobj.start()
+time.sleep(1)
+nodeobj.start()
+time.sleep(2)
+runcode.start()
+
+hubobj.join()
+nodeobj.join()
+runcode.join()
+
---
+++ Tests/RunTestInGrid.py
@@ -0,0 +1,67 @@
+import time
+from pprint import pprint
+
+from selenium import webdriver
+from selenium.webdriver.chromium.options import ChromiumOptions
+
+from Pages.source_destination_select import SourceDestinationSelect
+from Pages.calenderselect import CalenderHandle
+from Pages.classselection import ClassSelect
+from Pages.avaliable_Trains import AvaliableTrains
+from Utilites.collect_train_data import data_inot_csv
+import unittest
+
+class TestMakeMyTrip(unittest.TestCase):
+    def setUp(self) -> None:
+        chromeOptions = ChromiumOptions()
+        chromeOptions.set_capability("platformName", "Windows 10")
+        chromeOptions.set_capability("browserName", 'chrome')
+        self.driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub",
+                                  options=chromeOptions)
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+        self.driver.get("https://railways.makemytrip.com/railways/listing?classCode=&className=All%20Classes&date=20230220&destCity=Satna&destStn=STA&srcCity=Bhopal&srcStn=BPL&trainNumber=")
+
+    def test_Make_My_Trip(self):
+        driver=self.driver
+        sd=SourceDestinationSelect(driver)
+        # From city selection
+        sd.from_city_select('Katni')
+        # To City Selection
+        sd.to_city_select('Jabalpur')
+
+
+        # Calender handng
+        # trvdate=input('''Enter your Traveling date in format
+                # date monthname year
+                # Example: 20 February 2023: ''')
+        trvdate = '20 May 2023'
+        calender=CalenderHandle(driver)
+        calender.enter_date(trvdate)
+
+
+        # Select class
+        clselect=ClassSelect(driver)
+        clselect.class_select()
+        # click on Search button
+        clselect.search_train()
+
+
+        # Available trains data
+        avtrain=AvaliableTrains(driver)
+        traindata=avtrain.find_trains_data()
+
+
+        # convert train data into csv
+        data_inot_csv(traindata)
+
+        # Display Data
+        pprint(traindata)
+        time.sleep(5)
+
+    def tearDown(self) -> None:
+        self.driver.close()
+
+if __name__ == '__main__':
+    unittest.main()
+
